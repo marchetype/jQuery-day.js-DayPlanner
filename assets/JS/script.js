dfeeb6a651d90a2ba74b1b3492686a06ce2e3924
Mark Barstow <markbarstowjr@gmail.com>
2022-11-27 11:16:44 -0500

    grabbed currentDayEl from html.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,5 @@
+let currentDayEl = $('#currentDay');
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
